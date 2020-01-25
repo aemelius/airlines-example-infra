47afb614636715dabd567f7a16a8913a582b67c9
aemelius <simoneperfect@gmail.com>
2020-01-25 16:18:20 +0000

    Adding helm initialization

---
+++ 01_EKS/03_helm.sh
@@ -0,0 +1,26 @@
+export RBAC_FILE=rbac.yaml
+
+cat <<EoF > ${RBAC_FILE}
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: tiller
+  namespace: kube-system
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: tiller
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+  - kind: ServiceAccount
+    name: tiller
+    namespace: kube-system
+EoF
+
+kubectl apply -f ./${RBAC_FILE}
+helm init --service-account tiller
